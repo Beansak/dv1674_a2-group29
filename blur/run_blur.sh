c538092de073b038d581a2523916378e881e8d90
Isak Klaesson <129725851+Beansak@users.noreply.github.com>
2025-09-26 16:08:10 +0200

    bash file now runs callgrind on im1 and img2

--- blur/run_blur.sh
+++ blur/run_blur.sh
@@ -8,3 +8,10 @@ for img in "${images[@]}"; do
     /usr/bin/time -v ./blur 15 "data/$img" "$output" 2>&1 | grep -E "Percent of CPU this job got|Elapsed \(wall clock\)|Maximum resident set size|Major \(requiring I/O\) page faults|Minor \(reclaiming a frame\) page faults|Voluntary context switches|Involuntary context switches|File system inputs|File system outputs|Page size|Exit status"
     echo "-----------------------------------------"
 done
+
+# Run valgrind only after all timing is done, for all images
+
+echo "Running valgrind (callgrind) on img1 and img2..."
+valgrind --tool=callgrind ./blur 15 "data/img1.ppm" "output_img1.ppm"
+valgrind --tool=callgrind ./blur 15 "data/img2.ppm" "output_img2.ppm"
+echo "-----------------------------------------"
