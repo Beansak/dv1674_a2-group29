cf2df2f852bd433aefd1c0c307e26f36b6ab24cd
Isak Klaesson <129725851+Beansak@users.noreply.github.com>
2025-09-29 14:54:36 +0200

    verify_baseline done

--- blur/verify_baseline.sh
+++ blur/verify_baseline.sh
@@ -10,14 +10,16 @@ for image in im1 im2 im3 im4
     do
         ./blur 15 "data/$image.ppm" "./data_o/blur_${image}_test.ppm"
 
+
         if ! cmp -s "./data_o/blur_${image}.ppm" "./data_o/blur_${image}_test.ppm"
         then
             echo "${red}Error: Incongruent output data detected when blurring image $image.ppm ${reset}"
             status=1
+        else
+            echo "Output matches for $image.ppm"
         fi
 
         rm "./data_o/blur_${image}_test.ppm"
     done
-done
 
 exit $status
