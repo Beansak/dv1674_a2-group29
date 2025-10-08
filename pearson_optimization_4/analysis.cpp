659eeaed3080ece18887329bee0840b6dad6ebe5
anton <anton.m.lindstrom@gmail.com>
2025-10-08 19:58:15 +0200

    big improvement in analysis.cpp

--- pearson_optimization_4/analysis.cpp
+++ pearson_optimization_4/analysis.cpp
@@ -27,47 +27,25 @@ namespace Analysis
             means[i] = datasets[i].mean();
         }
 
-        // precompute magnitudes
-        std::vector<double> magnitudes(n);
-        std::vector<Vector> mm_v(n);
+        // precompute normalized vectors (mean-subtracted and divided by magnitude)
+        std::vector<Vector> normalized;
+        normalized.reserve(n);
 
         for (size_t i = 0; i < n; i++)
         {
-            magnitudes[i] = (datasets[i] - means[i]).magnitude();
-            mm_v[i] = (datasets[i] - means[i]);
+            auto mm = datasets[i] - means[i];
+            auto mag = mm.magnitude();
+            normalized.push_back(mm / mag); // Store the fully normalized vector
         }
 
         for (size_t sample1 = 0; sample1 < n - 1; sample1++)
         {
             for (size_t sample2 = sample1 + 1; sample2 < n; sample2++)
             {
-                auto x_mm_over_x_mag = mm_v[sample1] / magnitudes[sample1];
-                auto y_mm_over_y_mag = mm_v[sample2] / magnitudes[sample2];
-
-                auto r = x_mm_over_x_mag.dot(y_mm_over_y_mag);
+                auto r = normalized[sample1].dot(normalized[sample2]);
                 result.push_back(std::max(std::min(r, 1.0), -1.0));
             }
         }
         return result;
     }
-
-    // Note: This function is not used in the current implementation
-    double pearson(Vector vec1, Vector vec2)
-    {
-        auto x_mean{vec1.mean()};
-        auto y_mean{vec2.mean()};
-
-        auto x_mm{vec1 - x_mean};
-        auto y_mm{vec2 - y_mean};
-
-        auto x_mag{x_mm.magnitude()};
-        auto y_mag{y_mm.magnitude()};
-
-        auto x_mm_over_x_mag{x_mm / x_mag};
-        auto y_mm_over_y_mag{y_mm / y_mag};
-
-        auto r{x_mm_over_x_mag.dot(y_mm_over_y_mag)};
-
-        return std::max(std::min(r, 1.0), -1.0);
-    }
-};
+}
