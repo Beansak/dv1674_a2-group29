0d63c9f5d3c2f92e9f0f0ee7d98b9365813fb0a7
Isak Klaesson <129725851+Beansak@users.noreply.github.com>
2025-10-09 15:45:31 +0200

    pthreads update 1 - not working

--- blur_parralellize/filters.cpp
+++ blur_parralellize/filters.cpp
@@ -50,88 +50,154 @@ namespace Filter
 
     Matrix blur(Matrix m, const int radius, const int nr_of_threads)
     {
-        Matrix scratch{PPM::max_dimension};
-        auto dst{m};
 
         double w[Gauss::max_radius]{}; // we create an array to hold the weights
         Gauss::get_weights(radius, w);
 
-        for (auto y{0}; y < dst.get_y_size(); y++)
+        Matrix scratch{PPM::max_dimension};
+        auto dst{m};
+
+        // [I] Pthreads variables
+        pthread_t threads[nr_of_threads];
+        int thread_ids[nr_of_threads];
+
+
+        int rows_per_thread = m.get_y_size() / nr_of_threads;
+        int remaining_rows = m.get_y_size() % nr_of_threads;
+        
+        
+        
+
+        // [I] Create a struct to hold the data for each thread
+        struct ThreadData thread_data[nr_of_threads];
+
+        // [I] Initialize thread IDs
+        for (int i = 0; i < nr_of_threads; i++)
         {
-            for (auto x{0}; x < dst.get_x_size(); x++)
-            {
-
-                // unsigned char Matrix::r(unsigned x, unsigned y) const
-                // {
-                //     return R[y * x_size + x];
-                // }
-
-                // [A] define rgb and n (normalization factor) with the center pixel
-                auto r{w[0] * dst.r(x, y)}, g{w[0] * dst.g(x, y)}, b{w[0] * dst.b(x, y)}, n{w[0]};
-
-                // [A] loop through the weights and add the weighted values of the surrounding pixels
-                for (auto wi{1}; wi <= radius; wi++)
-                {
-                    auto wc{w[wi]};
-                    auto x2{x - wi};
-
-                    // [A] check bounds and add the weighted pixel values to rgb and n
-                    if (x2 >= 0)
-                    {
-                        r += wc * dst.r(x2, y);
-                        g += wc * dst.g(x2, y);
-                        b += wc * dst.b(x2, y);
-                        n += wc;
-                    }
-                    x2 = x + wi;
-                    if (x2 < dst.get_x_size())
-                    {
-                        r += wc * dst.r(x2, y);
-                        g += wc * dst.g(x2, y);
-                        b += wc * dst.b(x2, y);
-                        n += wc;
-                    }
-                }
-                scratch.r(x, y) = r / n;
-                scratch.g(x, y) = g / n;
-                scratch.b(x, y) = b / n;
-            }
+            thread_ids[i] = i;
         }
 
-        for (auto y{0}; y < dst.get_y_size(); y++)
+        // [I] Create the threads
+        for (int i = 0; i < nr_of_threads; i++)
         {
-            for (auto x{0}; x < dst.get_x_size(); x++)
+            thread_data[i].src = &scratch;
+            thread_data[i].dst = &dst;
+            thread_data[i].weights = w;
+            thread_data[i].radius = radius;
+            thread_data[i].start_row = i * rows_per_thread;
+            thread_data[i].end_row = (i + 1) * rows_per_thread;
+
+            if (i == nr_of_threads - 1)
             {
-
-                auto r{w[0] * scratch.r(x, y)}, g{w[0] * scratch.g(x, y)}, b{w[0] * scratch.b(x, y)}, n{w[0]};
-
-                for (auto wi{1}; wi <= radius; wi++)
-                {
-                    auto wc{w[wi]};
-                    auto y2{y - wi};
-                    if (y2 >= 0)
-                    {
-                        r += wc * scratch.r(x, y2);
-                        g += wc * scratch.g(x, y2);
-                        b += wc * scratch.b(x, y2);
-                        n += wc;
-                    }
-                    y2 = y + wi;
-                    if (y2 < dst.get_y_size())
-                    {
-                        r += wc * scratch.r(x, y2);
-                        g += wc * scratch.g(x, y2);
-                        b += wc * scratch.b(x, y2);
-                        n += wc;
-                    }
-                }
-                dst.r(x, y) = r / n;
-                dst.g(x, y) = g / n;
-                dst.b(x, y) = b / n;
+                // [I] Last thread takes the remaining rows
+                thread_data[i].end_row += remaining_rows; 
             }
+
+            pthread_create(&threads[i], nullptr, thread_function, &thread_data[i]);
+        }
+
+        
+
+        // [I] Wait for the threads to finish
+        for (int i = 0; i < nr_of_threads; i++)
+        {
+            pthread_join(threads[i], nullptr);
         }
 
         return dst;
+
     }
+
+
+        // [I] Thread function        
+    void *thread_function(void *arg)
+        {
+            ThreadData *data = static_cast<ThreadData *>(arg);
+            Matrix *src = data->src;
+            Matrix *dst = data->dst;
+            double *w = data->weights;
+            int radius = data->radius;
+            int start_row = data->start_row;
+            int end_row = data->end_row;
+
+
+            
+
+            for (auto y{start_row}; y < end_row; y++)
+            {
+                for (auto x{0}; x < dst->get_x_size(); x++)
+                {
+
+                    // unsigned char Matrix::r(unsigned x, unsigned y) const
+                    // {
+                    //     return R[y * x_size + x];
+                    // }
+
+                    // [A] define rgb and n (normalization factor) with the center pixel
+                    auto r{w[0] * dst->r(x, y)}, g{w[0] * dst->g(x, y)}, b{w[0] * dst->b(x, y)}, n{w[0]};
+
+                    // [A] loop through the weights and add the weighted values of the surrounding pixels
+                    for (auto wi{1}; wi <= radius; wi++)
+                    {
+                        auto wc{w[wi]};
+                        auto x2{x - wi};
+
+                        // [A] check bounds and add the weighted pixel values to rgb and n
+                        if (x2 >= 0)
+                        {
+                            r += wc * dst->r(x2, y);
+                            g += wc * dst->g(x2, y);
+                            b += wc * dst->b(x2, y);
+                            n += wc;
+                        }
+                        x2 = x + wi;
+                        if (x2 < dst->get_x_size())
+                        {
+                            r += wc * dst->r(x2, y);
+                            g += wc * dst->g(x2, y);
+                            b += wc * dst->b(x2, y);
+                            n += wc;
+                        }
+                    }
+                    scratch.r(x, y) = r / n;
+                    scratch.g(x, y) = g / n;
+                    scratch.b(x, y) = b / n;
+                }
+            }
+
+            for (auto y{0}; y < dst->get_y_size(); y++)
+            {
+                for (auto x{0}; x < dst->get_x_size(); x++)
+                {
+
+                    auto r{w[0] * scratch.r(x, y)}, g{w[0] * scratch.g(x, y)}, b{w[0] * scratch.b(x, y)}, n{w[0]};
+
+                    for (auto wi{1}; wi <= radius; wi++)
+                    {
+                        auto wc{w[wi]};
+                        auto y2{y - wi};
+                        if (y2 >= 0)
+                        {
+                            r += wc * scratch.r(x, y2);
+                            g += wc * scratch.g(x, y2);
+                            b += wc * scratch.b(x, y2);
+                            n += wc;
+                        }
+                        y2 = y + wi;
+                        if (y2 < dst->get_y_size())
+                        {
+                            r += wc * scratch.r(x, y2);
+                            g += wc * scratch.g(x, y2);
+                            b += wc * scratch.b(x, y2);
+                            n += wc;
+                        }
+                    }
+                    dst->r(x, y) = r / n;
+                    dst->g(x, y) = g / n;
+                    dst->b(x, y) = b / n;
+                }
+            }
+            return nullptr;
+        }
 
 }
--- blur_parralellize/filters.hpp
+++ blur_parralellize/filters.hpp
@@ -9,7 +9,17 @@ Author: David Holmqvist <daae19@student.bth.se>
 
 namespace Filter
 {
-
+    // [I] Create a struct to hold the data for each thread
+    struct ThreadData
+    {
+        Matrix *src;
+        Matrix *dst;
+        double *weights;
+        int radius;
+        int start_row;
+        int end_row;
+    };
+    
     namespace Gauss
     {
         constexpr unsigned max_radius{1000};
@@ -17,8 +27,9 @@ namespace Filter
         constexpr float pi{3.14159};
 
         void get_weights(int n, double *weights_out);
+        
     }
-
+    void *thread_function(void *arg);
     Matrix blur(Matrix m, const int radius, const int nr_of_threads);
 
 }
