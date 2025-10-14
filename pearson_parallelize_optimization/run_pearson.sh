26a5b284f7b700f29a6cf197dd2932ee8a83f5bf
Isak Klaesson <129725851+Beansak@users.noreply.github.com>
2025-10-14 16:02:06 +0200

    run_pearson changed to do threads

--- pearson_parallelize_optimization/run_pearson.sh
+++ pearson_parallelize_optimization/run_pearson.sh
@@ -6,18 +6,14 @@ make clean > /dev/null 2>&1
 make > /dev/null 2>&1
 
 datas=("128.data" "256.data" "512.data" "1024.data")
-
-for data in "${datas[@]}"; do
+threads=("1" "2" "4" "8" "16" "32")
+for thread_count in "${threads[@]}"; do
+    for data in "${datas[@]}"; do
     output="output_${data}.txt"
-    echo "Running pearson on $data..."
-    /usr/bin/time -v ./pearson "data/$data" "$output" 2>&1 | grep -E "Percent of CPU this job got|Elapsed \(wall clock\)|Maximum resident set size|Major \(requiring I/O\) page faults|Minor \(reclaiming a frame\) page faults|Voluntary context switches|Involuntary context switches|File system inputs|File system outputs|Page size|Exit status"
+    echo "Running pearson on $data for $thread_count threads..."
+    /usr/bin/time -v ./pearson_par "data/$data" "$output" "$thread_count" 2>&1 | grep -E "Percent of CPU this job got|Elapsed \(wall clock\)|Maximum resident set size|Major \(requiring I/O\) page faults|Minor \(reclaiming a frame\) page faults|Voluntary context switches|Involuntary context switches|File system inputs|File system outputs|Page size|Exit status"
     echo "-----------------------------------------"
+    done
+    echo "========================================="
 done
 
-# Run valgrind only after all timing is done, for all images
-for data in "${datas[@]}"; do
-    output="output_${data}.txt"
-    echo "Running valgrind on $data..."
-    MALLOC_MMAP_THRESHOLD_=0 valgrind --tool=callgrind ./pearson "data/$data" "$output"
-    echo "-----------------------------------------"
-done
